97cde871d81047e7e4132990ee98837585d84f8b
nichotelo <nicolaschristianto@mail.ugm.ac.id>
2020-11-06 23:32:22 +0700

    maze world

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nichotelo/ros/empty_ws/src
+# Build directory: /home/nichotelo/ros/empty_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_simulations")
